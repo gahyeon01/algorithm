c0cb98704770820519d92e7e7579efb852868c5d
gahyeon <bananajoge@gmail.com>
2024-09-16 23:07:12 +0900

    study4

---
+++ src/algorithm/ArrayAndList/Baekjoon2018.java
@@ -0,0 +1,36 @@
+package algorithm.ArrayAndList;
+
+import java.util.Scanner;
+
+public class Baekjoon2018 {
+    public static void main(String[] args) {
+        //투포인터 문제
+        //연속된 자연수의 합 구하기
+        /*
+        * 어떠한 자연수 N은 몇 개의 연속된 자연수의 합으로 나타낼 수 있다.
+        * 당신은 어떤 자연수 N을 몇 개의 연속된 자연수의 합으로 나타내는 가짓수를 알고 싶다.
+        * 이때 사용하는 자연수는 N이어야 한다.
+        * 연속된 수로 이뤄저야 한다는 점!
+        * 예를 들어, 10이라면 10, 1+2+3+4
+        * */
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int count = 1;
+        int start_index =  1;
+        int end_index = 1;
+        int sum = 1;
+
+        while(end_index != N) {
+            if(sum==N){
+                count++; end_index++; sum = sum + end_index;
+            } else if(sum > N){
+                sum = sum - start_index;
+                start_index++;
+            } else{
+                end_index++; sum = sum + end_index;
+            }
+        }
+        System.out.println(count);
+
+    }
+}
